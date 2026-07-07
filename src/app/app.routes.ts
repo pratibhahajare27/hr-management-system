0f082e52a847bfc48c706a0b865caf080da43b96
pratibhahajare27 <pratibhabhajare@gmail.com>
2026-07-07 16:10:48 +0530

    update leaves

--- src/app/app.routes.ts
+++ src/app/app.routes.ts
@@ -2,8 +2,10 @@ import { Routes } from '@angular/router';
 
 import { EmployeeList } from './features/employee-list/employee-list';
 import { Dashboard } from './features/dashboard/dashboard';
+import { LeaveList } from './features/leave-list/leave-list';
 
 export const routes: Routes = [
     { path: "", component: Dashboard },
-    { path: "employee", component: EmployeeList }
+    { path: "employee", component: EmployeeList },
+    { path: "leaves", component: LeaveList }
 ];
--- src/app/features/dashboard/dashboard.ts
+++ src/app/features/dashboard/dashboard.ts
@@ -1,4 +1,6 @@
-import { Component, signal } from '@angular/core';
+import { Component, inject, signal } from '@angular/core';
+import { Employee } from '../../services/employee';
+import { Leave } from '../../services/leave';
 
 @Component({
   selector: 'app-dashboard',
@@ -7,6 +9,28 @@ import { Component, signal } from '@angular/core';
   styleUrl: './dashboard.css',
 })
 export class Dashboard {
+  private employeeService = inject(Employee);
+  private leaveService = inject(Leave);
+
   totalEmployees = signal<number>(0);
   totalLeaves = signal<number>(0);
+  activeChart = signal<'employees' | 'leaves' | null>(null);
+
+  ngOnInit() {
+    this.employeeService.getEmplyees().subscribe(data => {
+
+    }
+    )
+    this.leaveService.getLeaves().subscribe(data => {
+      this.totalEmployees.set(data.length);
+    })
+  }
+  showEmpChart() {
+    this.activeChart.set('employees');
+  }
+
+  showLeaveChart() {
+    this.activeChart.set('leaves');
+  }
+
 }
